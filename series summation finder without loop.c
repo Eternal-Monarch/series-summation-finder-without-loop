754c86d85ebde9dcd5508984d57c5b046739a2ba
SubhasishDutta-Coder <58630896+SubhasishDutta-Coder@users.noreply.github.com>
2020-03-26 01:47:49 +0600

    Add files via upload

---
+++ series summation finder without loop.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{int a,b,c,d,n;
+printf("enter the last digit of sequal\n");
+scanf("%d",&n);
+b=((n*(n+1))/2);
+printf("the total summation of sequal is %d\n",b);
+return 0;
+}
